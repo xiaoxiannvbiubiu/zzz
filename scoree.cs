365d9a88be2047b211384c8b9d58b6bad0bd5b35
xiaoxiannvbiubiu <2826904752@qq.comj>
2016-09-26 10:32:11 +0800

    socre
    public int score=1;

---
+++ scoree.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoree : MonoBehaviour {
+	public int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI ()
+	{
+		GUI.skin.label.fontSize = 30;
+		string content = "fenshu:" + score.ToString ();
+		GUI.Label (new Rect (0, 0, Screen.width / 5, Screen.height / 8), content);
+	}
+}
+	
